9275ae9cab7bf659dfb7a74d33e9d03adb3f6ffa
R-man3000 <rasmus.luha@gmail.com>
2026-05-30 21:27:06 +0300

    add docker dns fix

--- README.md
+++ README.md
@@ -3,5 +3,13 @@ docker
 kubectl
 k3d
 
+### Docker connectivity
+For me, using `Fedora`, i was not able to access internet from my k8s cluster initally.
+The fix was adding dns in `/etc/docker/daemon.json`
+```
+{
+  "dns": ["8.8.8.8", "1.1.1.1"],
+   ...
+```
 
 ## TODO
--- setup_script.sh
+++ setup_script.sh
@@ -4,6 +4,5 @@ k3d cluster create mycluster --agents 2 -p "8080:80@loadbalancer" --k3s-arg "--d
 ## Install ingress
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/cloud/deploy.yaml
 
-
 ## Create namespaces
-kubectl creat ns heimdall
+kubectl create ns heimdall
