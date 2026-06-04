65063159edf96fdd8bc40ee4c3bb4ee5f2c583af
R-man3000 <rasmus.luha@gmail.com>
2026-06-04 23:17:19 +0300

    add prometheus monitoring for kubelab-app

--- setup_script.sh
+++ setup_script.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+echo "Staring cluster setup.. This may take a while (~10-15 min)"
+
 ## Create cluster
 k3d cluster create mycluster --agents 2 -p "8080:80@loadbalancer" --k3s-arg "--disable=traefik@server:*"
 
@@ -8,7 +10,7 @@ k3d cluster create mycluster --agents 2 -p "8080:80@loadbalancer" --k3s-arg "--d
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/cloud/deploy.yaml
 
 ## Wait for ingress-nginx to be ready
-kubectl wait --namespace ingress-nginx --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=120s
+kubectl wait --namespace ingress-nginx --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=300s
 
 ## Install ArgoCD
 ./bootstrap/install-argocd.sh
